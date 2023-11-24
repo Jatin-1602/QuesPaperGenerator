b01466e178db94b30b0c98807287ee33165ee0fd
Jatin Gupta <jatingupta6171@gmail.com>
2023-11-24 10:46:12 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import "bootstrap/dist/css/bootstrap.css";
 
 import Home from "./components/Home";
 import GetPaper from "./components/GetPaper";
+import AddQues from "./components/AddQues";
+import DisplayPaper from "./components/DisplayPaper";
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/getPaper" element={<GetPaper />}></Route>
+        <Route path="/addQues" element={<AddQues />}></Route>
+        <Route path="/displayPaper" element={<DisplayPaper />}></Route>
       </Routes>
     </div>
   );
